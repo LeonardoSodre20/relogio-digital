d302fd08cdd20b26502006c6646a117826e4ce73
Leonardo Sodré <LeonardoSodre25@outlook.com>
2022-01-02 23:03:42 -0300

    relogio digital

---
+++ app.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector('.clock-container');
+
+const updateClock = () =>{
+    const present = new Date();
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+
+    const clockHTML = `
+
+        <span class="clock-items" >${String(hours).length === 1 ? `0${hours}` : hours}</span> :
+        <span class="clock-items" >${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
+        <span class="clock-items" >${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
+    
+    `
+
+    clockContainer.innerHTML = clockHTML;
+}
+
+
+
+setInterval(updateClock, 1000);
